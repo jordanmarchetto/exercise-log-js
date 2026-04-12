91328b23869dc7bf8682b29e91a3df7efe5a1bb1
Jordan Marchetto <jordan.marchetto@gmail.com>
2026-04-12 13:55:52 -0400

    add get route

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,9 +1,25 @@
 const router = require('express').Router();
 const { pool } = require('../db');
 
+//TODO: add logging for every route
+//TODO: move queries to separate file
+//TODO: moar routes
+
 router.get('/', async (_req, res) => {
   try {
     const result = await pool.query('SELECT * from exercises');
+    res.json({ exercises: result.rows });
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      message: 'Internal server error'
+    });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const result = await pool.query(`SELECT * from exercises where id = $1`, [req.params.id]); //this is totally safe, right??? right???
     res.json({ exercises: result.rows });
   } catch (error) {
     res.status(500).json({
