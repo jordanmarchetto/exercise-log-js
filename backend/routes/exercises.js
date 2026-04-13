98848eaa90eee31d0d6548ef098c22e8505468de
Jordan Marchetto <jordan.marchetto@gmail.com>
2026-04-12 22:01:34 -0400

    add simple error handling

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -10,7 +10,7 @@ router.get('/', async (_req, res) => {
   } catch (error) {
     res.status(500).json({
       status: 'error',
-      message: 'Internal server error'
+      message: error.message || 'Internal server error'
     });
   }
 });
@@ -18,12 +18,18 @@ router.get('/', async (_req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const result = await exercises.getExerciseById(req.params.id);
+    if (!result) {
+      return res.status(404).json({
+        status: 'error',
+        message: 'Exercise not found'
+      });
+    }
     const exercise = serializeExercise(result); 
     res.json(exercise);
   } catch (error) {
     res.status(500).json({
       status: 'error',
-      message: 'Internal server error'
+      message: error.message || 'Internal server error'
     });
   }
 });
--- backend/serializers/exercises.js
+++ backend/serializers/exercises.js
@@ -1,3 +1,15 @@
+/*
+	"id": "2",
+	"name": "Bench Press",
+	"description": null,
+	"icon": "bi-trophy-fill",
+	"set_types": [
+		"WeightWorkoutSet"
+	],
+	"created_at": "2024-06-24T03:32:32.164Z",
+	"updated_at": "2024-06-24T03:32:32.164Z",
+	"show_on_records": true
+*/
 const serializeExercises = (rows) => {
     return rows.map(serializeExercise);
 }
